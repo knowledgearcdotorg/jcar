2395d3177aa4d690db365202ef1b0e62fb76532b
Hayden Young <hayden@knowledgearc.com>
2015-04-22 18:12:21 +0200

    Build/Parse SEF urls whether or not there is a menu item. Menu item for all archived items retrieved by JCAR. Id is appended to the url built.

--- components/com_jcar/site/router.php
+++ components/com_jcar/site/router.php
@@ -8,24 +8,82 @@
  */
 defined('_JEXEC') or die;
 
+/**
+ * Rewrites urls to be SEF-friendly and understandable by Joomla.
+ */
 class JCarRouter extends JComponentRouterBase
 {
+    /**
+     * Builds the route for the com_contact component
+     *
+     * @param   array  &$query  An array of URL arguments
+     *
+     * @return  array  The URL arguments to use to assemble the subsequent URL.
+     */
     public function build(&$query)
     {
-        return array();
+        $segments = array();
+
+        // Get a menu item based on Itemid or currently active
+        $app = JFactory::getApplication();
+        $menu = $app->getMenu();
+
+        // We need a menu item.  Either the one specified in the query, or the current active one if none specified
+        if ($itemId = JArrayHelper::getValue($query, 'Itemid')) {
+            $menuItem = $menu->getActive();
+        } else {
+            $menuItem = $menu->getItem($itemId);
+        }
+
+        $mView = JArrayHelper::getValue($menuItem->query, 'view');
+
+        if ($view = JArrayHelper::getValue($query, 'view')) {
+            if (!$itemId || empty($menuItem) || $menuItem->component != 'com_jcar') {
+                $segments[] = $view;
+            }
+
+            if ($id = JArrayHelper::getValue($query, 'id')) {
+                $segments[] = $id;
+            }
+
+            unset($query['view']);
+            unset($query['id']);
+        }
+
+        return $segments;
     }
 
+    /**
+     * Parses the segments of a URL.
+     *
+     * @param   array  &$segments  The segments of the URL to parse.
+     *
+     * @return  array  The URL attributes to be used by the application.
+     */
     public function parse(&$segments)
     {
-        return array();
+        $vars = array();
+
+        $total = count($segments);
+
+        $item = $this->menu->getActive();
+
+        if (isset($item)) {
+            $vars['view'] = JArrayHelper::getValue($item->query, 'view');
+        } else {
+            $vars['view'] = JArrayHelper::getValue($segments, 0);
+        }
+
+        $vars['id'] = JArrayHelper::getValue($segments, $total-1);
+
+        return $vars;
     }
 }
 
 /**
  * JCar router functions
  *
- * These functions are proxys for the new router interface
- * for old SEF extensions.
+ * These functions are proxies for the new router interface for old SEF extensions.
  *
  * @deprecated  4.0  Use Class based routers instead
  */
