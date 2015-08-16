4db125124fe79396a0380d399e0f9dabec63a2aa
Hayden Young <hayden@knowledgearc.com>
2015-08-16 18:58:21 +0200

    Output a category and its items as json.

---
+++ components/com_jcar/site/views/category/view.json.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @package     JCar.Component
+ * @subpackage  Site
+ *
+ * @copyright   Copyright (C) 2015 KnowledgeArc Ltd. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+defined('_JEXEC') or die;
+
+/**
+ * Displays a category and all its sub-categories from an archive.
+ */
+class JCarViewCategory extends JViewLegacy
+{
+    protected $item;
+
+    /**
+     * Diplay a category and all its sub-categories.
+     *
+     * @param   string  $tpl  The name of the template file to parse; automatically searches through the template paths.
+     *
+     * @return  mixed  A string if successful, otherwise a Error object.
+     */
+    public function display($tpl = null)
+    {
+        $this->item = $this->get('Item');
+
+        echo json_encode($this->item);
+    }
+}
