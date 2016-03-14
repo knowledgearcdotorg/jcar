ad115f9f6f3f79f48d07a48a49d82b6dcae01924
haydenyoung <hayden@knowledgearc.com>
2016-03-14 15:27:47 +0800

    Update copyright.

--- components/com_jcar/site/models/categories.php
+++ components/com_jcar/site/models/categories.php
@@ -3,7 +3,7 @@
  * @package     JCar.Component
  * @subpackage  Site
  *
- * @copyright   Copyright (C) 2015 KnowledgeArc Ltd. All rights reserved.
+ * @copyright   Copyright (C) 2015-2016 KnowledgeArc Ltd. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 defined('_JEXEC') or die;
