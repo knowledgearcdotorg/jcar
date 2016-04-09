d98c59a846348d0b755a55fc091911de5c59c1d8
haydenyoung <hayden@knowledgearc.com>
2016-04-09 21:46:59 +0800

    Include jcar.js file in template.

--- components/com_jcar/site/views/category/tmpl/loadmore.php
+++ components/com_jcar/site/views/category/tmpl/loadmore.php
@@ -7,6 +7,8 @@
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 defined('_JEXEC') or die;
+
+JFactory::getDocument()->addScript(JUri::root().'/media/com_jcar/js/jcar.js');
 ?>
 
 <section id="jcarCategory">
