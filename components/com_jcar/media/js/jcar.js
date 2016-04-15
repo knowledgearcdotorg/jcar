0c0749d73b1eb968180efb5d00fc69a11b3c4d3a
pushpan999 <getpushpan@gmail.com>
2016-04-15 17:02:50 +0545

    jcar ajax dynamic get url fixed

--- components/com_jcar/media/js/jcar.js
+++ components/com_jcar/media/js/jcar.js
@@ -1,49 +1,31 @@
 (function ($) {
     $(document).ready(function () {
-        (function () {
-            $.get("index.php/dspace-more-items-example?token=oai_dc///com_10049_25/100&amp;start=100&amp;format=json", function (data) {
-                console.log(data);
-                var totalData = data.pagination.total;
-                console.log(totalData); //check the total number of data returned
-                // console.log(typeof totalData); //check if the returned value is number
+        var getURL = $(".jcar-load-more").data("url");
 
-                $(".jcar-load-more").click(function (e) {
-                     e.preventDefault();
-                    var i;
-                    var totalDataCounter;
-                    for (i = 100; i < totalData; i = i + 100) {
-                        totalDataCounter = i;
-                    }
-
-                    var myURL = "index.php/dspace-more-items-example?token=oai_dc///com_10049_25/" +
-                        totalDataCounter + "&amp;start=" + totalDataCounter + "&amp;format=json";
-
-                    $.get(myURL, function (response) {
-                        var totalDataLoopCount = response.pagination.limitstart + response.items.length;
-                        // console.log(totalData);
-                        // console.log(totalDataLoopCount);
-                        // console.log(response);
-                         var dataList = response.items;                        
-                         var currentURL = $(location).attr('href');
-                            for (var j = 0; j < dataList.length; j++) {
+        $(".jcar-load-more").click(function (e) {
+            //console.log("before running loop: " + getURL);
+            if (getURL === "?format=json") {
+                $(".jcar-load-more").attr("disabled", "disabled").html("No more data to dispaly");
+            }
+            else {
+                $.get(getURL, function (data) {
+                    //console.log(data);
+                    var dataList = data.items;
+                    var currentURL = $(location).attr('href');
+                    for (var j = 0; j < dataList.length; j++) {
                                 //console.log(dataList[j].name);
                                 var dataHtml = '<h2><a href="'+  currentURL + '/item/' + dataList[j].id +'">'+ dataList[j].name +'</a></h2>';   
                                  $( "articles#jcar-lists" ).append(dataHtml);                       
                             }
-                           
-                        if (totalData === totalDataLoopCount) {
-                            $(".jcar-load-more").attr("disabled", "disabled").html("No more data to dispaly");
-                        } 
-                        // else {
-                        //     $(".jcar-load-more").show();                          
-                        // }
-                    });
-                });
+                    var updatedURL = data.pagination.pagesNext;
+                    getURL = updatedURL;
+                    //console.log("end of loop: " + getURL);
+                    //    $(".jcar-load-more").data( "url", updatedURL);
+                    //     console.log(getURL);
+                }, "json");
+            }
 
-            }, "json");
-        })();
-
-
+        });
     });
 
 })(jQuery);
