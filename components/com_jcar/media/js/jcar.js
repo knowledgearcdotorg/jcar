ab1c910adfa760e206e5fe76be3b61154cfbc08e
pushpan999 <getpushpan@gmail.com>
2016-04-16 01:18:09 +0545

    Result Count Update

--- components/com_jcar/media/js/jcar.js
+++ components/com_jcar/media/js/jcar.js
@@ -1,40 +1,41 @@
 (function ($) {
-     $(document).ajaxStart(function () {
-            //$( ".log" ).text( "Triggered ajaxStart handler." );
-            //console.log("request started");
-             $(".jcar-load-more").attr("disabled", "disabled").html("<span class=\"loader-gif\"></span> " + "Loading Records..");
-            // $(".loader-gif").css("display", "block");
-        });
-        $(document).ajaxStop(function () {
-            // $( ".log" ).text( "Triggered ajaxStop handler." );
-            //console.log("request completed");
-             $(".jcar-load-more").removeAttr("disabled").html("Load more");
-            // $(".loader-gif").css("display", "none");
-        });
+    $(document).ajaxStart(function () {
+        //$( ".log" ).text( "Triggered ajaxStart handler." );
+        //console.log("request started");
+        $(".jcar-load-more").attr("disabled", "disabled").html("<span class=\"loader-gif\"></span> " + "Loading Records..");
+        // $(".loader-gif").css("display", "block");
+    });
+    $(document).ajaxStop(function () {
+        // $( ".log" ).text( "Triggered ajaxStop handler." );
+        //console.log("request completed");
+        $(".jcar-load-more").removeAttr("disabled").html("Load more");
+        // $(".loader-gif").css("display", "none");
+    });
     $(document).ready(function () {
-       
+
         var getURL = $(".jcar-load-more").data("url");
-       // console.log("before running loop: " + getURL);
+        // console.log("before running loop: " + getURL);
 
         $(".jcar-load-more").click(function (e) {
             e.preventDefault();
-           // console.log("before running inside loop: " + getURL);
+            // console.log("before running inside loop: " + getURL);
             // if (getURL === "?format=json") {
             //     $(".jcar-load-more").hide();
             // }
             // else {
             $.get(getURL, function (data) {
-                //console.log(data);
+                console.log(data);
                 var dataList = data.items;
-               
+
                 for (var j = 0; j < dataList.length; j++) {
                     //console.log(dataList[j].name);
-                    var dataHtml = '<h2><a href="' + dataList[j].link +'">' + dataList[j].name + '</a></h2>';
+                    var dataHtml = '<h2><a href="' + dataList[j].link + '">' + dataList[j].name + '</a></h2>';
                     $("articles#jcar-lists").append(dataHtml);
                 }
+                $("section#jcarCategory > header").html("<h1></h1><div></div>Results 1 - " + (data.pagination.limitstart + dataList.length) + " of " + data.pagination.total);
                 var updatedURL = data.pagination.pagesNext;
                 getURL = updatedURL;
-               // console.log("end of loop: " + getURL);
+                // console.log("end of loop: " + getURL);
                 //    $(".jcar-load-more").data( "url", updatedURL);
                 //     console.log(getURL);
                 if (getURL === "?format=json") {
